356b398244cb9711b9d40561141ba2489fba5e09
yuvaraja-08 <yuvaraja8824@gmail.com>
2026-09-09 23:44:39 +0530

    day3

---
+++ Day3/Day3_testcase1.java
@@ -0,0 +1,53 @@
+package Day1Assigment.Day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Day3_testcase1 {
+      public static void main(String[] args) throws InterruptedException {
+    	  //notification disabled 
+    	 ChromeOptions options = new ChromeOptions();
+    	 options.addArguments("--disable-notifications");
+    	WebDriver driver = new ChromeDriver(options);
+  		driver.manage().window().maximize();
+  		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+  		driver.get("https://www.easemytrip.com/");
+  		//-------------------------------from----------------------------------
+  		driver.findElement(By.xpath("//input[@id='FromSector_show']")).click();
+  		driver.findElement(By.xpath("//li[@onclick=\"autoSelectMul('spn2', 'FromSector_show', 'BLR-Bangalore, India','airport2');\"]")).click();
+  		//------------------------------To---------------------------------------
+  		Thread.sleep(3000);
+  		
+  		driver.findElement(By.xpath("//input[@id=\"a_Editbox13_show\"]")).sendKeys("Goa");
+  		driver.findElement(By.xpath("//li[@onclick=\"autoSelectMul('spn5', 'Editbox13_show', 'GOI-Goa, India','airport5');\"]")).click();
+  		
+  		Thread.sleep(3000);
+  		//--------------------------date-----------------------------------------
+  	    driver.findElement(By.xpath("//li[@id='trd_0_13/09/2026']")).click();
+  		Thread.sleep(3000);
+//  		driver.findElement(By.xpath("//div[@id='divRtnCal']")).click();
+//  		driver.findElement(By.xpath("//li[@id='fst_4_01/10/2026']")).click();
+  		//---------------------------------------add 2 adults keep economy----------------
+  		
+  		driver.findElement(By.xpath("//div[@id='myFunction4']")).click();
+  		driver.findElement(By.xpath("(//button[@id='add'])[1]")).click();
+  		
+  		//--------------------------click on search button-----------------------
+  		Thread.sleep(3000);
+  		driver.findElement(By.xpath("//div[@class='fss_flex search_colm']")).click();
+  		
+  		//-------------------validate flight result page--------------------
+  		if(driver.getTitle().contains("EaseMyTrip.com Lowest Airfare, Flight Tickets, Cheap Air Tickets")) {
+  			System.out.println("Flight result page is didplayed");
+  		}else {
+  			System.out.println("Flight result page is not displayed");
+  		}
+  		
+  		
+  		
+	}
+}
---
+++ Day3/Day3_testcase2.java
@@ -0,0 +1,44 @@
+package Day1Assigment.Day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day3_testcase2 {
+     public static void main(String[] args) throws InterruptedException {
+    	// Launch The browser
+         WebDriver driver = new ChromeDriver();
+         //maximize browser
+         driver.manage().window().maximize();
+         //implicity wait 
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+         //naviagte to the browser
+         driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
+         Thread.sleep(3000);
+         WebElement tt=driver.findElement(By.xpath("//input[@placeholder='Select A Date']"));
+         tt.click();
+         
+         //--------------------navigate to next month----------------------
+         driver.findElement(By.xpath("//button[@class='react-datepicker__navigation react-datepicker__navigation--next']")).click();
+         
+         //---------------------click on date------------------------------
+         driver.findElement(By.xpath("//div[@aria-label=\"Choose Thursday, October 8th, 2026\"]")).click();
+         
+         Thread.sleep(3000);
+         System.out.println(tt.getDomProperty("value"));
+         
+         //-------------------validate displayed date correct or not---------------------
+         
+         String actuldate = tt.getDomProperty("value");
+         
+         if(actuldate.equals("08/10/2026")) {
+        	 System.out.println("Displayed correct Date");
+         }else {
+        	 System.out.println("Displayed invaild date");
+         }
+         
+	}
+}
